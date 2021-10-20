227012b3ee2f5cb6f3a0a034c09ad1995fe38cbc
adrianartale <23aartale@athenian.org>
2021-10-19 20:28:16 -0700

    this commit is fire

--- src/main/java/Mathey.java
+++ src/main/java/Mathey.java
@@ -8,7 +8,14 @@ public class Mathey {
      * public static ...
      */
     // YOUR CODE HERE, METHOD HEADER ALSO REQUIRED
-
+    public static int max(int x, int y){
+        if (x > y) {
+            return x;
+        }
+        else {
+            return y;
+        }
+    }
 
 
 
@@ -16,7 +23,14 @@ public class Mathey {
      * Ex. max(1.2, 4.0) => 4.0
      */
     // YOUR CODE HERE, METHOD HEADER ALSO REQUIRED
-
+    public static double max(double x, double y){
+        if (x > y) {
+            return x;
+        }
+        else {
+            return y;
+        }
+    }
 
 
 
@@ -24,7 +38,9 @@ public class Mathey {
      * Ex. max(1, 4, 2) => 4
      */
     // YOUR CODE HERE, METHOD HEADER ALSO REQUIRED
-
+    public static int max(int x, int y, int z){
+        return max(max(x, y), z);
+    }
 
 
 
@@ -32,7 +48,9 @@ public class Mathey {
      * Ex. max(1.0, 4.25, 1.3, 2.1) => 4.25
      */
     // YOUR CODE HERE, METHOD HEADER ALSO REQUIRED
-
+    public static double max(double w, double x, double y, double z){
+        return max(max(max(x, y), z), w);
+    }
 
 
 
@@ -45,7 +63,9 @@ public class Mathey {
      *     randomInteger(1, 4) => 4
      */
     // YOUR CODE HERE, METHOD HEADER ALSO REQUIRED
-
+    public static int randomInteger(int x, int y){
+        return (int) Math.random()* (1-y+x) + x;
+    }
 
 
 
@@ -57,7 +77,9 @@ public class Mathey {
      *     randomInteger(5) => 0
      */
     // YOUR CODE HERE, METHOD HEADER ALSO REQUIRED
-
+    public static int randomInteger(int x){
+        return (int) (Math.random()*(x+1));
+    }
 
 
 
@@ -73,7 +95,13 @@ public class Mathey {
      *     pow(3, 4) => 81
      */
     // YOUR CODE HERE, METHOD HEADER ALSO REQUIRED
-
+    public static int pow(int x, int y){
+        int answer = 1;
+        for (int i = 1; i <= x; i++){
+            answer *= y;
+        }
+        return answer;
+    }
 
 
 
@@ -83,7 +111,14 @@ public class Mathey {
      *     abs(-2) => 2
      */
     // YOUR CODE HERE, METHOD HEADER ALSO REQUIRED
-
+    public static int abs(int x){
+        if (x>=0){
+            return x;
+        }
+        else{
+            return x*-1;
+        }
+    }
 
 
 
@@ -91,10 +126,13 @@ public class Mathey {
      * integer value that you get by rounding that double
      * You may assume that the integer is positive
      * Ex. round(2.4) => 2
-     *     round(2.5) => 2
+     *     round(2.5) => 3
      */
     // YOUR CODE HERE, METHOD HEADER ALSO REQUIRED
-
+    public static int round(double x){
+        x += 0.5;
+        return (int) x;
+    }
 
 
 
@@ -108,7 +146,9 @@ public class Mathey {
      *     floor(2.999999999999) => 2
      */
     // YOUR CODE HERE, METHOD HEADER ALSO REQUIRED
-
+    public static int floor(double x){
+        return (int) x;
+    }
 
 
 
@@ -122,7 +162,10 @@ public class Mathey {
      *     ceil(3.01) => 4
      */
     // YOUR CODE HERE, METHOD HEADER ALSO REQUIRED
-
+    public static int ceil(double x){
+        x += 0.9;
+        return (int) x;
+    }
 
 
 
--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,8 +4,8 @@ public class MyMain {
 
     // Generates a random number between 10 and 20, inclusive
     public static int randomTeen() {
-        // YOUR CODE HERE FOR WARMUP
-        return -1;
+        int x = (int) (Math.random()*11) + 10;
+        return x;
     }
 
     // Use your previous method to generate three random numbers between 10 and 20, inclusive.
@@ -16,16 +16,55 @@ public class MyMain {
     // The largest number is 20
     // The smallest number is 10
     public static void main(String[] args) {
-        // YOUR CODE HERE FOR WARMUP
-
-
-
-
-
+//        int w = randomTeen();
+//        int x = randomTeen();
+//        int y = randomTeen();
+//        int z = randomTeen();
+//        System.out.println(x);
+//        System.out.println(y);
+//        System.out.println(z);
+//        System.out.println(Mathey.max(x, y, z));
+//        System.out.println(Mathey.max(x, y));
+//        System.out.println(Mathey.max(w,x,y,z));
+//        System.out.println(Mathey.randomInteger(x));
+//        System.out.println(Mathey.randomInteger(x, y));
+//
+//
+//
+//
+//
 //         // Uncomment this code later!
-//         System.out.println("Mathey.max tests");
-//         System.out.println(Mathey.max(1, 2)); // 2
-//         System.out.println(Mathey.max(2, 1)); // 2
+         System.out.println("Mathey.max tests");
+         System.out.println(Mathey.max(1, 2));
+         System.out.println(Mathey.max(2, 1));
+
+        System.out.println("Mathey.RandomInteger tests");
+        System.out.println(Mathey.randomInteger(3,8));
+        System.out.println(Mathey.randomInteger(1,6));
+
+        System.out.println("Mathey.pow tests");
+        System.out.println(Mathey.pow(3,6));
+        System.out.println(Mathey.pow(2,5));
+
+        System.out.println("Mathey.abs tests");
+        System.out.println(Mathey.abs(5));
+        System.out.println(Mathey.abs(-2));
+
+        System.out.println("Mathey.round tests");
+        System.out.println(Mathey.round(6.9));
+        System.out.println(Mathey.round(4.1));
+
+        System.out.println("Mathey.floor tests");
+        System.out.println(Mathey.floor(2.5));
+        System.out.println(Mathey.floor(9.2));
+
+        System.out.println("Mathey.ceil tests");
+        System.out.println(Mathey.ceil(6.7));
+        System.out.println(Mathey.ceil(9.01));
+
+        System.out.println("Mathey.sqrt tests");
+        System.out.println(Mathey.sqrt(9));
+        System.out.println(Mathey.sqrt(16));
     }
 
 }
